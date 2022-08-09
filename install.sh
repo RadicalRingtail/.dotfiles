09487ef42a0a3c9d2f1105ac4b7d2d773446cf55
Ringtail <29079415+RadicalRingtail@users.noreply.github.com>
2022-08-09 17:56:58 -0400

    probably makes more sense to put it in this order

--- install.sh
+++ install.sh
@@ -3,6 +3,14 @@
 # install xcode tools
 xcode-select --install
 
+# install homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# install packages from Brewfile
+brew bundle install ~/.dotfiles/Brewfile
+
+#todo: auto install jetbrains mono/mono nerd font
+
 # update preferences
 /bin/bash ./defaults.sh
 
@@ -13,11 +21,3 @@ touch ~/.hushlogin
 ln -s ~/.dotfiles/zsh/.zshrc ~/.zshrc
 ln -s ~/.dotfiles/git/.gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/yt-dlp/yt-dlp.conf ~/yt-dlp.conf
-
-# install homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-# install packages from Brewfile
-brew bundle install ~/.dotfiles/Brewfile
-
-#todo: auto install jetbrains mono/mono nerd font
