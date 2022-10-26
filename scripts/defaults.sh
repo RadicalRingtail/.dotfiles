6fb52637274ac41daae0f7ff5e5e49a106b8c2f6
Ringtail <29079415+RadicalRingtail@users.noreply.github.com>
2022-10-26 19:59:08 -0400

    found out you can install ableton via homebrew, my life is changed

--- scripts/defaults.sh
+++ scripts/defaults.sh
@@ -13,6 +13,7 @@ defaults write com.apple.Dock "mineffect" -string "genie"
 defaults write NSGlobalDomain "AppleShowAllExtensions" -bool "true"
 defaults write com.apple.Finder "AppleShowAllFiles" -bool "true"
 defaults write com.apple.Finder "FXEnableExtensionChangeWarning" -bool "false"
+defaults write com.apple.finder "FXPreferredViewStyle" -string "Nlsv"
 
 # Terminal (theres most likely a better way to do this)
 #curl 'https://raw.githubusercontent.com/lysyi3m/macos-terminal-themes/master/themes/VS%20Code%20Dark%20Plus.terminal' -o theme.terminal
