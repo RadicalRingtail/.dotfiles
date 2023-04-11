0ce9c5afc9a758cd6c32077089d4d44906bde636
Ringtail <29079415+RadicalRingtail@users.noreply.github.com>
2023-04-10 22:04:40 -0400

    fixed blender config

--- scripts/blender_config.sh
+++ scripts/blender_config.sh
@@ -1,6 +1,6 @@
-cd ~/Library/ApplicationSupport/Blender
+cd ~/Library/Application\ Support/Blender
 
 latest=$(ls | sort -nr | head -1)
 
-cp ~/.dotfiles/blender/startup.blend ~/Library/ApplicationSupport/Blender/$latest/config/startup.blend
-cp ~/.dotfiles/blender/userpref.blend ~/Library/ApplicationSupport/Blender/$latest/config/userpref.blend
+cp ~/.dotfiles/blender/startup.blend ~/Library/Application\ Support/Blender/$latest/config/startup.blend
+cp ~/.dotfiles/blender/userpref.blend ~/Library/Application\ Support/Blender/$latest/config/userpref.blend
