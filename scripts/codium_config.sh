84e2ade462034a5f893a2568fca7f405950f0bd0
Ringtail <29079415+RadicalRingtail@users.noreply.github.com>
2024-04-25 23:14:44 -0400

    much needed updates

--- scripts/codium_config.sh
+++ scripts/codium_config.sh
@@ -1,12 +1,11 @@
 plugins=(ms-python.python 
-ms-toolsai.jupyter 
-ms-vscode.live-server 
-opensumi.opensumi-default-themes 
-ritwickdey.LiveServer 
-rust-lang.rust 
-tamasfe.even-better-toml)
+ms-python.python
+ms-python.vscode-pylance
+ms-vscode.live-server
+opensumi.opensumi-default-themes
+geequlim.godot-tools)
 
 for i in "${plugins[@]}" 
 do
-    codium --install-extension $i
+    code --install-extension $i
 done
